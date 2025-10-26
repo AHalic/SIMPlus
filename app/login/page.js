02254eaf86cac631c2934e7f1f6f50e8bb96e13f
AHalic <sophie.gama@aluno.ufes.br>
2025-10-26 13:05:47 -0400

    ✨ Adds Login page

--- app/login/page.js
+++ app/login/page.js
@@ -1,10 +1,113 @@
-// localhost/login
+"use client";
+
+import CustomInput from "@/components/Input";
+import { EmailOutlined, LockOutline } from "@mui/icons-material";
+import { Button, Grid, InputAdornment, Typography } from "@mui/material";
+import Image from "next/image";
+
+export default function Login() {
+    const handleSignIn = () => {
+        // TODO: handle signing
+    }
 
 
-// axios.get()
-
-export default async function Login() {
     return (
-        <h1>Login!</h1>
+        <Grid container 
+            width="100%"
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            paddingY="3rem"
+            gap="2rem"
+        >
+            {/* logo */}
+            <Grid container 
+                direction="column"
+                alignItems="center"
+                justifyContent="center"
+                gap="1.5rem"
+            >
+                <Grid>
+                    <Image
+                        unselectable="on" 
+                        src={`/icon.svg`}
+                        height={80}
+                        width={80}
+                        alt="logo"
+                    />
+                </Grid>
+
+                <Grid container direction="column" alignItems="center">
+                    <Typography variant="h4" fontWeight={600}>
+                        SIM+
+                    </Typography>
+
+                    <Typography variant="subtitle1" color="text.secondary">
+                        Manage your inventory with ease
+                    </Typography>
+                </Grid>
+            </Grid>
+
+            {/* main form */}
+            <Grid 
+                container
+                bgcolor="secondary.main"
+                borderRadius="12px"
+                padding="2.5rem"
+                direction="column"
+                boxShadow={1}
+                gap="2rem"
+                width="60vh"
+                maxWidth="800px"
+            >
+                <Grid>
+                    <Typography variant="h6">
+                        Sign in to your account
+                    </Typography>
+                </Grid>
+
+                {/* inputs */}
+                <Grid container direction="column" gap="1rem">
+                    <Grid width="100%">
+                        <CustomInput 
+                            label="Email" 
+                            placeholder="you@email.com"
+                            slotProps={{
+                                input: {
+                                    startAdornment: (
+                                    <InputAdornment position="start">
+                                        <EmailOutlined sx={{ color: "divider" }} />
+                                    </InputAdornment>
+                                    ),
+                                },
+                            }}
+                        />
+                    </Grid>
+
+                    <Grid width="100%">
+                        <CustomInput 
+                            label="Password" 
+                            placeholder="••••"
+                            type="password"
+                            slotProps={{
+                                input: {
+                                    startAdornment: (
+                                    <InputAdornment position="start">
+                                        <LockOutline sx={{ color: "divider" }} />
+                                    </InputAdornment>
+                                    ),
+                                },
+                            }}
+                        />
+                    </Grid>                    
+                </Grid>
+
+                <Grid container>
+                    <Button onClick={(e) => handleSignIn()} variant="contained" fullWidth>
+                        Sign in
+                    </Button>
+                </Grid>
+            </Grid>
+        </Grid>
     )
 }
