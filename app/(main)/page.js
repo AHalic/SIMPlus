bdd3d317bd3ed4715083d3336ef31ff3e2dd23aa
AHalic <sophie.gama@aluno.ufes.br>
2025-10-30 20:47:20 -0400

    ✨ Adds stock listing page

--- app/(main)/page.js
+++ app/(main)/page.js
@@ -1,97 +1,102 @@
 "use client";
 
-import { Grid, Typography, Box, Button } from "@mui/material";
-import CustomInput from "@/components/Input";
+import StockListing, { StockListingSkeleton } from "@/components/StockListing";
+import { StyledSelect, StyledInput } from "@/components/StyledInputs";
+import { Search } from "@mui/icons-material";
+import { Grid } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+
 
 export default function Home() {
-  return (
-    <Grid
-      container
-      justifyContent="normal"
-      alignItems="center"
-      direction="column"
-      spacing={3}
-      sx={{
-        minHeight: "85vh",
-        padding: "2rem",
-        textAlign: "center",
-        backgroundColor: "background.default",
-      }}
-    >
-      <Grid item>
-        <Typography
-          variant="h3"
-          fontWeight="700"
-          color="text.primary"
-        
-        >
-          Welcome to SIM+
-        </Typography>
-        <Typography variant="body1" color="text.secondary">
-          Your all-in-one stock control and management system
-        </Typography>
-      </Grid>
-
-      {/* Search bar 
-          needs to be connected to backend
-      */}
-      <Grid
-        item
-        sx={{
-          width: "50%",
-          maxWidth: "400px",
-          mt: 1,
-        }}
-      >
-        <CustomInput
-          label="Search Products"
-          placeholder="Enter product name..."
-        />
-      </Grid>
+	const [departments, setDepartments] = useState([])
+	const [filterSelected, setFilterSelected] = useState({department: "", id: ""})
+	const [stock, setStock] = useState([])
+	const [loadingStock, setLoadingStock] = useState(true)
 
 
-      {/*visuals go here 
-        e.g., charts, recent activity, etc.
-      */}
-      <Grid item>
+	// fetch departments
+	useEffect(()=> {
+		axios.get('/api/department')
+			.then((response) => {
+				setDepartments(response.data.departments)
+			})
+			.catch((error) => {
+				console.log(error)
+			})
+	}, [])
 
-      </Grid>
-      <Grid
-        item
-        sx={{
-          display: "flex",
-          gap: 1,
-          mt: 1,
-        }}
-      >
-        {/* Action Buttons 
-          needs to be connected to backend
-        */}
-        <Button
-          variant="contained"
-          color="primary"
-          sx={{ fontWeight: "600", paddingX: 3 }}
-          
-        >
-          View Inventory
-        </Button>
-        <Button
-          variant="contained"
-          color="primary"
-          sx={{ fontWeight: "600", paddingX: 3 }}
-        >
-          Add New Product
-        </Button>
-      </Grid>
 
-      
-        
-      <Box sx={{ position: "absolute", bottom: 24 }}>
-        <Typography variant="caption" color="text.secondary">
-          © {new Date().getFullYear()} SIMPlus | Stock Control System
-        </Typography>
-      </Box>
-    </Grid>
-  );
+	// fetch stock
+	useEffect(() => {
+		setLoadingStock(true)
+		
+		axios.get('/api/stock', {params: filterSelected})
+		.then((response) => {
+				setLoadingStock(false)
+				console.log(response.data)
+				setStock(response.data.stock)
+			})
+			.catch((error) => {
+				console.log(error)
+				setLoadingStock(false)
+			})
+	}, [filterSelected])
+
+
+    return (
+		<Grid 
+			container
+			direction="column"
+			padding="40px 36px"
+			spacing={2}
+		>
+			{/* Search bar */}
+			<Grid 
+				container
+				spacing={2}
+				sx={{ 
+					padding: "20px",
+					backgroundColor:"secondary.main",
+					borderRadius: "12px",
+				}}
+			>
+				<Grid size={{ xs:4 }}>
+					<StyledSelect 
+						label="Filter By Department" 
+						options={[
+							{value: "", label: "All"},
+							...departments?.map((i) => (
+								{value: i.dept_id, label: i.dept_name}
+							))
+						]
+						}
+						onChange={(e) => {
+							setFilterSelected(prev => ({...prev, department: e.target.value}))}
+						}
+						value={filterSelected.department}
+					/>
+				</Grid>
+
+				<Grid size={{ xs:8 }}>
+					<StyledInput 
+						label="Search By SKU"
+						endAdornment={
+							<Search sx={{ color: "divider" }} />
+						}
+						value={filterSelected.id}
+					/>
+				</Grid>
+			</Grid>
+
+
+			{loadingStock ? (
+				<StockListingSkeleton />
+			) : (
+				<StockListing stock={stock} />
+			)}
+			
+		</Grid>
+    );
 }
-
