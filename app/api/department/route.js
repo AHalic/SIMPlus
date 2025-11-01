b078a9df79a523edf82b6346e51394d2bc9c822b
AHalic <sophie.gama@aluno.ufes.br>
2025-10-31 20:16:42 -0400

    🔥 Remove mock data

--- app/api/department/route.js
+++ app/api/department/route.js
@@ -3,30 +3,6 @@ import Department from "../../../models/Department.js";
 import mongoose from "mongoose";
 import "dotenv/config";
 
-/** 
- * Mock promise to get data
- * */ 
-const mock = async () => {
-
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            const success = true; 
-    
-            if (success) {
-                resolve({
-                    departments: [
-                        {dept_id: 1, dept_name: "Home"}, 
-                        {dept_id: 2, dept_name: "Clothing"}
-                    ]
-                });
-            } else {
-                reject("Error: Failed to fetch data."); 
-            }
-        }, 500); 
-    });
-}
-
-
 /**
  * Department GET route 
  * @param {*} request 
@@ -51,16 +27,3 @@ export async function GET(request) {
         return NextResponse.json({ error: error.message }, { status: 500 });
     }
 }
-
-/*export async function GET(request) {
-    // TODO: this will be the GET route
-
-    return await mock().then((res) => {
-        return NextResponse.json(res, {
-            status: 201,
-            headers: { 'Content-Type': 'application/json' }
-        })
-    })
-    
-    
-}*/
