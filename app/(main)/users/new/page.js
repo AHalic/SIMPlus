4a53bf99c59baf67bc8f001ce30a0c627a928995
AHalic <sophie.gama@aluno.ufes.br>
2025-11-01 23:16:00 -0400

    ✨ Adds create employee page

--- app/(main)/users/new/page.js
+++ app/(main)/users/new/page.js
@@ -1,43 +1,89 @@
 "use client";
 
-import CustomInput from "@/components/Input";
+import { OutlinedInput, OutlinedSelect } from "@/components/OtulinedInput";
+import { RoleEnum } from "@/models/Role";
 import { Button, Grid, Typography } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 
+
+const defaultValues = {
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+    dept_id: "",
+    role: ""
+}
+
 export default function NewUser() {
+    const [departments, setDepartments] = useState([])
+    const [isSubmitLoading, setIsSubmitLoading] = useState(false)
+
     const {
         control,
         handleSubmit,
         formState: { errors },
-    } = useForm()
+        reset
+    } = useForm({
+        defaultValues
+    })
 
-    const onSubmit = (data) => {
-        // TODO: handle submit
+
+    const onSubmit = async (data) => {
+        setIsSubmitLoading(true)
+        
+        await axios.post('/api/employee', data)
+            .then((response) => {
+                    setIsSubmitLoading(false)
+                    reset(defaultValues)
+                })
+                .catch((error) => {
+                    // TODO: Add snackbar
+                    setIsSubmitLoading(false)
+                    console.log(error)
+                })        
     }
 
     const onCancel = () => {
-        // TODO: handle cancel
-        // navigate back or reset form
+        reset(defaultValues)
     }
+
+    useEffect(()=> {
+        axios.get('/api/department')
+            .then((response) => {
+                const transformedDept = response.data.departments?.map((i) => (
+                    {value: i._id, label: i.dept_name}
+                ))
+
+                setDepartments(transformedDept)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }, [])
+
 
 
     return (
         <Grid 
 			container
 			direction="column"
-			padding="36px 52px"
+			padding="42px 52px"
 			spacing={2}
 		>
             <Grid
                 container
-                padding="28px"
+                padding="52px 64px"
                 direction="column"
+                spacing={3}
                 sx={{
                     borderRadius: '12px',
                     backgroundColor: 'secondary.main'
                 }}
             >
-                <Typography variant="h6" fontWeight={600}>
+                <Typography variant="h5" fontWeight={600}>
                     Add New Employee
                 </Typography>
 
@@ -49,8 +95,9 @@ export default function NewUser() {
                         spacing={4}
                     >
                         {/* Form inputs */}
-                        <Grid container spacing={3} direction="column">
-                            <Grid container spacing={2}>
+                        <Grid container spacing={4} direction="column">
+                            {/* first last name inputs */}
+                            <Grid container spacing={6}>
                                 <Grid size={{ xs: 6 }}>
                                     <Controller
                                         name={`first_name`}
@@ -58,10 +105,10 @@ export default function NewUser() {
                                         rules={{ 
                                             required: "Field required", 
                                             maxLength: { value: 16, message: "Name has to be at most 16 characters" }, 
-                                            minLength: { value: 5, message: "Name has to be at least 5 characters" }
+                                            minLength: { value: 2, message: "Name has to be at least 2 characters" }
                                         }}
                                         render={({ field : { onChange, onBlur, value, ref } }) => (
-                                            <CustomInput 
+                                            <OutlinedInput 
                                                 error={!!errors.first_name}
                                                 helperText={errors.first_name?.message}
                                                 fullWidth
@@ -83,10 +130,10 @@ export default function NewUser() {
                                         rules={{ 
                                             required: "Field required", 
                                             maxLength: { value: 16, message: "Name has to be at most 16 characters" }, 
-                                            minLength: { value: 5, message: "Name has to be at least 5 characters" }
+                                            minLength: { value: 2, message: "Name has to be at least 2 characters" }
                                         }}
                                         render={({ field : { onChange, onBlur, value, ref } }) => (
-                                            <CustomInput 
+                                            <OutlinedInput 
                                                 error={!!errors.last_name}
                                                 helperText={errors.last_name?.message}
                                                 fullWidth
@@ -102,43 +149,18 @@ export default function NewUser() {
                                 </Grid>
                             </Grid>
 
-                            <Grid container spacing={2}>
-                                <Grid size={{ xs: 6 }}>
-                                    <Controller
-                                        name={`password`}
-                                        control={control}
-                                        rules={{ 
-                                            // TODO: validate format
-                                            required: "Field required", 
-                                            maxLength: { value: 20, message: "Password has to be at most 20 characters" }, 
-                                            minLength: { value: 8, message: "Password has to be at least 8 characters" }
-                                        }}
-                                        render={({ field : { onChange, onBlur, value, ref } }) => (
-                                            <CustomInput 
-                                                error={!!errors.password}
-                                                helperText={errors.password?.message}
-                                                fullWidth
-                                                onChange={onChange}
-                                                onBlur={onBlur}
-                                                value={value}
-                                                inputRef={ref}
-                                                label="First Name" 
-                                                lightLabel 
-                                            />
-                                        )}
-                                    />
-                                </Grid>
-
+                            {/* email - password inputs */}
+                            <Grid container spacing={6}>
                                 <Grid size={{ xs: 6 }}>
                                     <Controller
                                         name="email"
                                         control={control}
                                         rules={{ 
-                                            // TODO: validate format
                                             required: "Field required",
                                         }}                                        
                                         render={({ field : { onChange, onBlur, value, ref } }) => (
-                                            <CustomInput 
+                                            <OutlinedInput
+                                                type="email" 
                                                 error={!!errors.email}
                                                 helperText={errors.email?.message}
                                                 fullWidth
@@ -146,7 +168,40 @@ export default function NewUser() {
                                                 onBlur={onBlur}
                                                 value={value}
                                                 inputRef={ref}
-                                                label="Last Name" 
+                                                label="Email" 
+                                                lightLabel 
+                                            />
+                                        )}
+                                    />
+                                </Grid>
+
+                                <Grid size={{ xs: 6 }}>
+                                    <Controller
+                                        name={`password`}
+                                        control={control}
+                                        rules={{ 
+                                            required: "Field required", 
+                                            maxLength: { value: 20, message: "Password has to be at most 20 characters" }, 
+                                            minLength: { value: 8, message: "Password has to be at least 8 characters" },
+                                            validate: {
+                                                checkFormat: async (password) => {
+                                                    //  at least a letter, a special character and a number;
+                                                    const regex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[^A-Za-z\d]).+$/;
+
+                                                    if (!regex.test(password)) return "Password has to have at least a letter, a special character and a number"
+                                                },
+                                            }                                            
+                                        }}
+                                        render={({ field : { onChange, onBlur, value, ref } }) => (
+                                            <OutlinedInput 
+                                                error={!!errors.password}
+                                                helperText={errors.password?.message}
+                                                fullWidth
+                                                onChange={onChange}
+                                                onBlur={onBlur}
+                                                value={value}
+                                                inputRef={ref}
+                                                label="Password" 
                                                 lightLabel 
                                             />
                                         )}
@@ -154,19 +209,82 @@ export default function NewUser() {
                                 </Grid>
                             </Grid>
 
-                            {/* TODO: add inputs for role and dpt  */}
+                            {/* department - role inputs */}
+                            <Grid container spacing={6}>
+                                <Grid size={{ xs: 6 }}>
+                                    <Controller
+                                        name={`dept_id`}
+                                        control={control}
+                                        rules={{ 
+                                            required: "Field required",
+                                        }}
+                                        render={({ field : { onChange, onBlur, value, ref } }) => (
+                                            <OutlinedSelect 
+                                                error={!!errors.dept_id}
+                                                helperText={errors.dept_id?.message}
+                                                fullWidth
+                                                onChange={onChange}
+                                                onBlur={onBlur}
+                                                value={value}
+                                                inputRef={ref}
+                                                label="Department" 
+                                                lightLabel
+                                                options={departments}
+                                                variant="outlined" 
+                                            />
+                                        )}
+                                    />
+                                </Grid>
+
+                                <Grid size={{ xs: 6 }}>
+                                    <Controller
+                                        name={`role`}
+                                        control={control}
+                                        rules={{ 
+                                            required: "Field required",
+                                        }}
+                                        render={({ field : { onChange, onBlur, value, ref } }) => (
+                                            <OutlinedSelect 
+                                                error={!!errors.role}
+                                                helperText={errors.role?.message}
+                                                fullWidth
+                                                onChange={onChange}
+                                                onBlur={onBlur}
+                                                value={value}
+                                                inputRef={ref}
+                                                label="Role" 
+                                                lightLabel
+                                                options={RoleEnum.map((i) => ({value: i, label: i}))}
+                                                variant="outlined" 
+                                            />
+                                        )}
+                                    />
+                                </Grid>
+                            </Grid>                            
                         </Grid>
 
                         {/* Form button */}
                         <Grid container direction="row" justifyContent="end" spacing={3}>
                             <Grid size={{ xs: 3 }} >
-                                <Button variant="contained" onClick={onCancel} fullWidth sx={{ backgroundColor: 'divider'}}>
-                                    Cancel
+                                <Button 
+                                    variant="contained"
+                                    onClick={onCancel}
+                                    fullWidth
+                                    sx={{ backgroundColor: 'divider'}}
+                                    disabled={isSubmitLoading}
+                                >
+                                    Reset form
                                 </Button>
                             </Grid>
 
                             <Grid size={{ xs: 3 }}>
-                                <Button type="submit" variant="contained" fullWidth sx={{ backgroundColor: 'success.main'}}>
+                                <Button
+                                    type="submit"
+                                    variant="contained"
+                                    fullWidth
+                                    sx={{ backgroundColor: 'success.main'}}
+                                    disabled={isSubmitLoading}
+                                >
                                     Save
                                 </Button>
                             </Grid>
