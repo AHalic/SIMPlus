5ca78be7caa4f4ce47ef9947390587a8e350c4d0
JackTaormino <jtaorm1@students.towson.edu>
2025-11-02 23:14:44 -0500

    Items Sold process & some model changes

--- models/Item_Sold.js
+++ models/Item_Sold.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const Item_Sold = new mongoose.Schema({
         transaction_id: { type: mongoose.Schema.Types.ObjectId, ref: "Transaction", required: true },
         item_id: { type: mongoose.Schema.Types.ObjectId, ref: "Item", required: true },
-        amount_sold: { type: Number, required: true }
+        amount_sold: { type: Number, required: true },
+        sell_price: { type: Number, required: true }
     },
     { timestamps: true }
 );
